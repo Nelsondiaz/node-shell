3df94c16c1cda79fbb2be6552b792763655a0110
Siedrix <Siedrix@gmail.com>
2010-10-21 23:27:54 -0500

    dance

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-
-/**
- * Module dependencies.
- */
-
-//var connect = require('./../../lib/connect');
-
 var jade = require('jade'),
 	connect = require('connect'),
 	meryl = require('meryl');
-// Visit /style.css
 
-
+//Declare Jade as template, check other supported templates at meryl
+//http://coffeemate.github.com/meryl/
 var opts = {
   templateDir: 'templates',
   templateExt: '.jade',
@@ -18,25 +11,30 @@ var opts = {
     return jade.render(src, {locals: data});
   }
 };
-/*
-connect.createServer(
-    connect.staticProvider({ root: __dirname + '/public', cache: true })
-).listen(3000);
-*/
-meryl.plug(require('connect').staticProvider());
 
-meryl.h('GET /user/{yourname}?',
-    function (req, resp) {
-		resp.render('layout',
-			{content: 'home', context: {people: [req.params.yourname, 'alice', 'jane', 'meryl']}
-		});
-	}
+//Add Connect Static Provider
+meryl.p(
+	connect.staticProvider()
 );
 
+//Home it says hello...
+meryl.h('GET /', function(req, resp){
+	resp.send('Hello, This is a Loki Shell, start develop');	
+});
+
+//Render a Template name home, standar template example provided by meryl
+//Check template documentation on http://jade-lang.com/
+meryl.h('GET /user/{yourname}?',function (req, resp) {
+	resp.render('layout',
+		{content: 'home', context: {people: [req.params.yourname, 'alice', 'jane', 'meryl']}
+	});
+});
+
+//File static provider at /public
 meryl.h('GET /public/', function(req, resp) {
 	connect.staticProvider()
 });
 
+//Run Server
 meryl.run(opts);
-
 console.log('Connect server listening on port 3000');
